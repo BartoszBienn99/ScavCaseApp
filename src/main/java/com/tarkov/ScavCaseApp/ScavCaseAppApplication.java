d5397c94ad6da1eabbc3d482be6fc42a14f1644e
BartoszBienn <bartosz.bienn@gmail.com>
2023-05-18 13:14:30 +0200

    init

---
+++ src/main/java/com/tarkov/ScavCaseApp/ScavCaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.tarkov.ScavCaseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScavCaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScavCaseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarkov/ScavCaseApp/ScavCaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarkov.ScavCaseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScavCaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
