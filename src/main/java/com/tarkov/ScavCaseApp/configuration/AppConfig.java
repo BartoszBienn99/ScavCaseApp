fe86d604d2ce78c339314b1717a23cfb93555436
BartoszBienn <bartosz.bienn@gmail.com>
2023-05-18 16:10:15 +0200

    fixed app configuration

--- src/main/java/com/tarkov/ScavCaseApp/configuration/AppConfig.java
+++ src/main/java/com/tarkov/ScavCaseApp/configuration/AppConfig.java
@@ -3,6 +3,7 @@ package com.tarkov.ScavCaseApp.configuration;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -23,25 +24,35 @@ public class AppConfig {
 
     @Bean
     InitializingBean initializingBean(UserDetailsManager userDetailsManager) {
-        InitializingBean bean = () -> {
-            UserDetails bb = User
+        return  () -> {
+            UserDetails admin = User
                     .builder()
                     .passwordEncoder(password -> PasswordEncoderFactories.createDelegatingPasswordEncoder().encode(password))
                     .password("11217")
-                    .roles("User")
-                    .username("bb")
+                    .roles("Admin")
+                    .username("admin")
                     .build();
-            userDetailsManager.createUser(bb);
+            userDetailsManager.createUser(admin);
         };
-        return bean;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
+                .logout();
+
+        httpSecurity
+                .csrf()
+                .disable();
+
         httpSecurity
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/login")
+                .hasRole("Admin")
                 .anyRequest()
                 .authenticated();
         return httpSecurity.build();
--- src/main/java/com/tarkov/ScavCaseApp/controller/UserController.java
+++ src/main/java/com/tarkov/ScavCaseApp/controller/UserController.java
@@ -4,8 +4,10 @@ import com.tarkov.ScavCaseApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
